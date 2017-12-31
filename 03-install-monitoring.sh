0dce2e2068b6393e2841dc8fee77739e677693f1
Iosif <vighiosif@gmail.com>
2017-12-31 17:34:24 +0200

    [master] - install glances properly

--- 03-install-monitoring.sh
+++ 03-install-monitoring.sh
@@ -2,5 +2,5 @@ apt-get update
 apt-get install htop
 apt-get install nmon && export NMON=mndc
 
-# apt-get install python-pip
-# apt-get install glances
+apt-get install python-pip
+pip install glances
