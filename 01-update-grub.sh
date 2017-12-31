1156597d49eee1b6da3868da950af7e9f2ea2654
Iosif <vighiosif@gmail.com>
2017-12-31 19:30:46 +0200

    [master] - typo

--- 01-update-grub.sh
+++ 01-update-grub.sh
@@ -1,8 +1,8 @@
 # https://steemit.com/mining/@maebog/running-12x-gpu-on-the-biostar-tb250-btc-pro-with-simplemining-os
 
 # Auto
-sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=""/GRUB_CMDLINE_LINUX_DEFAULT="quiet pci=noaer"/g' etc/default/grub
-cat etc/default/grub
+sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=""/GRUB_CMDLINE_LINUX_DEFAULT="quiet pci=noaer"/g' /etc/default/grub
+cat /etc/default/grub
 
 # Manual
 # Change GRUB_CMDLINE_LINUX_DEFAULT="quiet pci=noaer" (this turns off Advance Error Reporting)
