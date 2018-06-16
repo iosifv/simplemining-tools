5130d1bcb42582d5aa0c9e89c258c5ce84adc538
Iosif Vigh <dev@iosifv.com>
2018-06-16 19:26:03 +0300

    [master] - update aliases

--- 02-update-password.sh
+++ 02-update-password.sh
@@ -1,6 +1,4 @@
 # password is the user email, change to 7y
 echo '  =======>  password for user MINER:'
 passwd miner
-echo '  =======>  password for user ROOT:'
-sudo passwd root
 echo '  =======>  change hostname for this rig with: sudo hostname miner[NUMBER]'
