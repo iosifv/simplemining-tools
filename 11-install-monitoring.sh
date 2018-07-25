29224e8fa422136d81ae856f8f039d902743eb54
Iosif <iosifv@users.noreply.github.com>
2018-07-25 12:57:28 +0300

    Update 11-install-monitoring.sh

--- 11-install-monitoring.sh
+++ 11-install-monitoring.sh
@@ -3,6 +3,9 @@ sudo apt-get install htop
 sudo apt-get install nmon && export NMON=mndc
 sudo apt-get install multitail
 
+# Used for benchmarking, needs gui
+sudo apt-get install glmark2
+
 # Install Glances
 # usage: glances, glances -w
 sudo apt-get install python-pip
